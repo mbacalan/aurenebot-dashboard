7cbb7b3e20957e6277bf1152312edfbe73974e4b
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-10-07 11:23:28 +0300

    (server) Add config.js to .gitignore

--- server/src/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  discordRedirectUrl: "http://localhost:8080/login",
-  discordUserUrl: "/users/@me",
-  discordGuildsUrl: "/users/@me/guilds",
-  corsOrigin: "http://localhost:8080",
-  botID: "",
-  botToken: ""
-};
