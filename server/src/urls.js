818241cb934fffc5bdf68f238cbb2d010a5df94f
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-10-07 11:19:03 +0300

    (server) Chnge urls.js to config.js

--- server/src/urls.js
+++
@@ -1,7 +0,0 @@
-exports.urls = {
-  discordTokenUrl: "https://discord.com/api/oauth2/token",
-  discordRedirectUrl: "http://localhost:8080/login",
-  discordUserUrl: "https://discord.com/api/users/@me",
-  discordGuildsUrl: "https://discord.com/api/users/@me/guilds",
-  corsOrigin: "http://localhost:8080"
-};
