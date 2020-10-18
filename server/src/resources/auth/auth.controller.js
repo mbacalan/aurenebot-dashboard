3db8614cfbf818c642f9757a549ffcf08b004fe8
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-10-18 12:25:53 +0300

    (server) Use classes for controllers

--- server/src/resources/auth/auth.controller.js
+++ server/src/resources/auth/auth.controller.js
@@ -4,47 +4,51 @@ const { config } = require("../../utils/config");
 
 const discordApi = discordAxios.getInstance();
 
-exports.login = async function(req, res) {
-  if (!req.body.code) {
-    return res.sendStatus(401);
+class Controller {
+  async login(req, res) {
+    if (!req.body.code) {
+      return res.sendStatus(401);
+    }
+
+    const form = new FormData();
+
+    form.append("client_id", process.env.CLIENT_ID);
+    form.append("client_secret", process.env.CLIENT_SECRET);
+    form.append("grant_type", "authorization_code");
+    form.append("code", req.body.code);
+    form.append("redirect_uri", config.discordRedirectUrl);
+    form.append("scope", "identify guilds");
+
+    try {
+      const { data: tokens } = await discordApi.post("/oauth2/token", form, { headers: form.getHeaders() });
+
+      discordAxios.setHeaders({ Authorization: `Bearer ${tokens.access_token}` });
+
+      const { data: user } = await discordApi.get(config.discordUserUrl);
+      let { data: guilds } = await discordApi.get(config.discordGuildsUrl);
+
+      guilds = guilds.filter(guild => guild.owner == true);
+
+      req.session.dTokens = tokens;
+      req.session.dUser = {
+        ...user,
+        guilds
+      };
+
+      return res.json(req.session.dUser);
+    } catch (err) {
+      console.log(err);
+
+      return res.sendStatus(500);
+    }
   }
 
-  const form = new FormData();
+  async logout(req, res) {
+    req.session.dTokens = null;
+    req.session.dUser = null;
 
-  form.append("client_id", process.env.CLIENT_ID);
-  form.append("client_secret", process.env.CLIENT_SECRET);
-  form.append("grant_type", "authorization_code");
-  form.append("code", req.body.code);
-  form.append("redirect_uri", config.discordRedirectUrl);
-  form.append("scope", "identify guilds");
-
-  try {
-    const { data: tokens } = await discordApi.post("/oauth2/token", form, { headers: form.getHeaders() });
-
-    discordAxios.setHeaders({ Authorization: `Bearer ${tokens.access_token}` });
-
-    const { data: user } = await discordApi.get(config.discordUserUrl);
-    let { data: guilds } = await discordApi.get(config.discordGuildsUrl);
-
-    guilds = guilds.filter(guild => guild.owner == true);
-
-    req.session.dTokens = tokens;
-    req.session.dUser = {
-      ...user,
-      guilds
-    };
-
-    return res.json(req.session.dUser);
-  } catch (err) {
-    console.log(err);
-
-    return res.sendStatus(500);
+    return res.sendStatus(200);
   }
-};
+}
 
-exports.logout = async function(req, res) {
-  req.session.dTokens = null;
-  req.session.dUser = null;
-
-  return res.sendStatus(200);
-};
+module.exports = new Controller();
